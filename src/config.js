448531670950dd5c76d833a67f1b7a4726c91227
Claudio Lopez <claudiols1979@hotmail.com>
2026-03-04 22:33:01 -0600

    fix(config): strip trailing slashes from API_URL to prevent 404s

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const isProduction = process.env.NODE_ENV === "production";
 
-const API_URL = isProduction ? process.env.REACT_APP_BACKEND_URL : "http://192.168.100.114:5000";
+const rawUrl = isProduction ? process.env.REACT_APP_BACKEND_URL : "http://192.168.100.114:5000";
+const API_URL = rawUrl ? rawUrl.replace(/\/+$/, '') : "";
 
 export default API_URL;
